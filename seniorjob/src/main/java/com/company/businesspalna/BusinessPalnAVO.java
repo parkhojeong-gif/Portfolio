8705dbe4fb21c088c66aa07fad5a70e58921d900
skar0 <skar0@DESKTOP-7VTEBGC>
2021-03-28 23:25:05 +0900

    전체 vo생성

---
+++ seniorjob/src/main/java/com/company/businesspalna/BusinessPalnAVO.java
@@ -0,0 +1,13 @@
+package com.company.businesspalna;
+
+import lombok.Data;
+
+@Data
+public class BusinessPalnAVO { //사업계획서a(필수)
+	private String seq;		//사업계획서 글번호
+	private String business_a;		//사업의 배경과 필요성
+	private String business_b;		//사업의 목적
+	private String business_c;		//사업 기대효과
+	private String id;		//작성자
+	private String collection;		//지도내용
+}
