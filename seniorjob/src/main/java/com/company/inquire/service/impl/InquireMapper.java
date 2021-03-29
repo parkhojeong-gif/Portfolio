a8137291bda45d2550238e740b3e058445f6ee58
aekgmla77 <aekgmla@naver.com>
2021-03-29 15:30:16 +0900

    공지사항, 문의사항

--- seniorjob/src/main/java/com/company/inquire/service/impl/InquireMapper.java
+++ seniorjob/src/main/java/com/company/inquire/service/impl/InquireMapper.java
@@ -1,5 +1,20 @@
 package com.company.inquire.service.impl;
 
-public interface InquireMapper {
+import java.util.List;
 
+import com.company.inquire.service.InquireVO;
+
+
+
+public interface InquireMapper {
+	//등록
+	public int insertInquire(InquireVO vo);
+	//수정
+	public int updateInquire(InquireVO vo);
+	//삭제
+	public int deleteInquire(InquireVO vo);
+	//단건조회
+	public InquireVO getInquire(InquireVO vo);
+	//전체 검색 조회
+	public List<InquireVO> getInquireList();
 }
--- seniorjob/src/main/java/com/company/mentoring_reviews/service/impl/Mentoring_ReviewsMapper.java
+++ seniorjob/src/main/java/com/company/mentoring_reviews/service/impl/Mentoring_ReviewsMapper.java
@@ -1,5 +1,19 @@
 package com.company.mentoring_reviews.service.impl;
 
-public interface Mentoring_ReviewsMapper {
+import java.util.List;
 
+import com.company.mentoring_reviews.service.Mentoring_ReviewsVO;
+
+
+public interface Mentoring_ReviewsMapper {
+	//등록
+	public int insertMentoring_Reviews(Mentoring_ReviewsVO vo);
+	//수정
+	public int updateMentoring_Reviews(Mentoring_ReviewsVO vo);
+	//삭제
+	public int deleteMentoring_Reviews(Mentoring_ReviewsVO vo);
+	//단건조회
+	public Mentoring_ReviewsVO getMentoring_Reviews(Mentoring_ReviewsVO vo);
+	//전체 검색 조회
+	public List<Mentoring_ReviewsVO> getMentoring_ReviewsList();
 }
