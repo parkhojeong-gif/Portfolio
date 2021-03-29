2718d98ec38a219c82628e520bccd91556fccc46
parkhojeong-gif <hojung9564@gmail.com>
2021-03-29 14:30:27 +0900

    users, 멘토링 후기, 고객센터, 문의하기, 문의답변 인터페이스 및 컨트롤러수정

--- seniorjob/src/main/java/com/company/inquire/controller/InquireController.java
+++ seniorjob/src/main/java/com/company/inquire/controller/InquireController.java
@@ -1,8 +1,44 @@
 package com.company.inquire.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.company.inquire.service.InquireVO;
+import com.company.inquire.service.impl.InquireMapper;
 
 @Controller
 public class InquireController {
-
+	@Autowired InquireMapper inquireMapper;
+	
+	@RequestMapping("/getInquireList")	//문의 리스트
+	public String getInquireList(Model model) {
+		model.addAttribute("list", inquireMapper.getInquireList());
+		return "/inquire/getInquireList";
+	}
+	@RequestMapping("/insertInquire") //문의 등록 폼
+	public String insertInquire(InquireVO vo) {
+		return "/inquire/insertInquire";
+	}
+	@RequestMapping("/insertInquire") //문의 등록
+	public String insertInquireProc(InquireVO vo) {
+		inquireMapper.insertInquire(vo);
+		return "redirect:/getInquireList";
+	}
+	@RequestMapping("/updateInquire")	//문의 수정폼
+	public String updateInquire(InquireVO vo, Model model) {
+		model.addAttribute("list", inquireMapper.getInquire(vo));
+		return "/inquire/updateInquire";
+	}
+	@RequestMapping("/updateInquire")	//문의 수정
+	public String updateInquireProc(InquireVO vo) {
+		inquireMapper.updateInquire(vo);
+		return "redirect:/getInquireList";
+	}
+	@RequestMapping("/deleteInquire")	//문의 삭제
+	public String deleteInquire(InquireVO vo) {
+		inquireMapper.deleteInquire(vo);
+		return "redirect:/getInquireList";
+	}
 }
