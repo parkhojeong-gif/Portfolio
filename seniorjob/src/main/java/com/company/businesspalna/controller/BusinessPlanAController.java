d1caf260bf276852b2dd601719e7de3bac1d8be0
notion7171 <hello.git.notion7171@gmail.com>
2021-03-29 16:14:59 +0900

    지운거복구_0329_1614_양소민

--- seniorjob/src/main/java/com/company/businesspalna/controller/BusinessPlanAController.java
+++ seniorjob/src/main/java/com/company/businesspalna/controller/BusinessPlanAController.java
@@ -1,8 +1,25 @@
 package com.company.businesspalna.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.company.businesspalna.service.BusinessPlanAService;
+import com.company.businesspalna.service.BusinessPalnAVO;
 
 @Controller
 public class BusinessPlanAController {
+
+	@Autowired BusinessPlanAService bpService;
+
+	@GetMapping("/getSearchBusinessPlanA")
+	public String getSearchBusinessPlanA(BusinessPalnAVO vo, Model model) {
+		model.addAttribute("list", bpService.getSearchBusinessPlanA(vo));
+		return "getSearchBusinessPlanA";
+	}
+
 
 }
--- seniorjob/src/main/webapp/WEB-INF/views/home.jsp
+++ seniorjob/src/main/webapp/WEB-INF/views/home.jsp
@@ -13,7 +13,7 @@ a<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <a href="mypageHome">마이페이지홈</a>
 <a href="resumeInsert">이력서 등록 폼</a>
 <a href="pwNotFound">비밀번호찾을수없다</a>
-
+<a href="getSearchBusinessPlanA">test</a>
 
 </body>
 </html></html>
