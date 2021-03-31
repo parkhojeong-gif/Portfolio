d92d77d5ba93c54cf7beb92a92060feeee7bec6d
notion7171 <hello.git.notion7171@gmail.com>
2021-03-31 10:48:39 +0900

    0331_10:48_스케쥴서비스커밋취소?_양소민

--- seniorjob/src/main/java/com/company/schedule/service/ScheduleService.java
+++ seniorjob/src/main/java/com/company/schedule/service/ScheduleService.java
@@ -1,5 +1,18 @@
 package com.company.schedule.service;
 
+import java.util.List;
+
+
 public interface ScheduleService {
+	
+	public int insertSchedule(BusinessPalnAVO vo);
+	
+	public int updateSchedule(BusinessPalnAVO vo);
+	
+	public int deleteSchedule(BusinessPalnAVO vo);
+	
+	public BusinessPalnAVO getSchedule(BusinessPalnAVO vo);
+	
+	public List<BusinessPalnAVO> getSearchSchedule(BusinessPalnAVO vo);
 
 }
